9542041b775b3345f1a639a6d94a8ab42c1804e3
Jesse Pernilä <jessepernila@gmail.com>
2022-05-17 08:27:34 +0300

    BarGraph for weekly sets

--- src/components/Graph/BarGraph.js
+++ src/components/Graph/BarGraph.js
@@ -4,7 +4,7 @@ import { colorPalette } from "../../store/rpeStrings";
 
 const BarGraph = (props) => {
   const { dataState, category } = props;
-  console.log(dataState, category);
+  
   const [graphBars, setGraphBars] = useState();
 
   // select week's entries
@@ -18,7 +18,7 @@ const BarGraph = (props) => {
           weeklyMass: [],
         });
         return entry.date.forEach((date, dateIndex) => {
-          if (date.week === 19) {
+          if (date.week === 20) {
             barGraphEntries[entryIndex].weeklySets.push(entry.sets[dateIndex]);
             barGraphEntries[entryIndex].weeklyMass.push(entry.mass[dateIndex]);
           }
@@ -33,22 +33,27 @@ const BarGraph = (props) => {
 
   if (graphBars) {
     return (
-      <div className={css.graphFlexContainer}>
+      <div className={`${css.graphFlexContainer} ${css.barGraphFlex}`}>
         {graphBars.map((entry) => {
-          const totalSets = entry.weeklySets.reduce((previousValue, currentValue) => previousValue + currentValue);
-          const barHeightMultiplyer = Math.round(totalSets/2.5) < 6 ? Math.round(totalSets/2.5) : 5;
+          const initialValue = 0;
+          const totalSets = entry.weeklySets.reduce(
+            (previousValue, currentValue) => previousValue + currentValue, initialValue
+          );
+          let barHeightMultiplyer =
+            Math.round(totalSets / 2) < 6 ? Math.round(totalSets / 2) : 5;
           return (
-            <div
-            className={css.barContainer} key={entry.name}>
-                <div
+            <div className={css.barContainer} key={entry.name}>
+              <div
                 className={css.bar}
-                  style={{
-                    backgroundColor: colorPalette[barHeightMultiplyer - 1],
-                    width: 50,
-                    height: barHeightMultiplyer*25
-                  }}
-                >{totalSets}</div>
-              <div>{entry.name}</div>
+                style={{
+                  backgroundColor: colorPalette[barHeightMultiplyer - 1],
+                  width: 50,
+                  height: barHeightMultiplyer * 25,
+                }}
+              >
+                {totalSets}
+              </div>
+              <div className={css.barName}>{entry.name}</div>
             </div>
           );
         })}
